745568e361d32bed33f5b66f2d501b50f1e6ff6e
rankofootball <50488324+rankofootball@users.noreply.github.com>
2024-05-23 15:28:31 +0200

    Update layer.py

--- src/peft/tuners/lora/layer.py
+++ src/peft/tuners/lora/layer.py
@@ -90,9 +90,7 @@ class LoraLayer(BaseTunerLayer):
         self.out_features = out_features
 
     def update_layer(
-        self, adapter_name, r, lora_alpha, lora_dropout, init_lora_weights, use_rslora, use_dora: bool = False,
-        predefined_matrices_A 
-    ):
+        self, adapter_name, r, lora_alpha, lora_dropout, init_lora_weights, use_rslora, predefined_matrices_A, use_dora: bool = False ):
         # This code works for linear layers, override for other layer types
         if r <= 0:
             raise ValueError(f"`r` should be a positive integer value but the value passed is {r}")
