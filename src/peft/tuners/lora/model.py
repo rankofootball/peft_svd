de2398eb0aeae823194e6c871e98e5ece9ad694a
rankofootball <50488324+rankofootball@users.noreply.github.com>
2024-06-19 13:52:46 +0200

    Update model.py

--- src/peft/tuners/lora/model.py
+++ src/peft/tuners/lora/model.py
@@ -275,17 +275,16 @@ class LoraModel(BaseTuner):
 
     def _mark_only_adapters_as_trainable(self, model: nn.Module) -> None:
         for n, p in model.named_parameters():
-            p.requires_grad = False
-
             if self.prefix not in n:
                 p.requires_grad = False
-        print ("in markkkk")
-#        for n, p in model.named_parameters():
-#            if "lora_A" in n:
-#                p.requires_grad = True
-#            if "lora_B" in n:
-#                p.requires_grad = False
-
+        print ("in mark1")
+        for n, p in model.named_parameters():
+            if "lora_A" in n:
+                p.requires_grad = True
+                print ("True:",n)
+            if "lora_B" in n:
+                p.requires_grad = False
+                print ("False", n)
 #        for active_adapter in self.active_adapters:
 #            bias = self.peft_config[active_adapter].bias
 #            if bias == "none":
