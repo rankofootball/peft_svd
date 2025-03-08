96de32246bb824842450dec6157c7b7708ecdb15
rankofootball <50488324+rankofootball@users.noreply.github.com>
2025-03-08 21:37:44 +0100

    Update model.py and adding printout

--- src/peft/tuners/lora/model.py
+++ src/peft/tuners/lora/model.py
@@ -138,7 +138,7 @@ class LoraModel(BaseTuner):
     def __init__(self, model, config, adapter_name) -> None:
 # seems the product is B times A
         super().__init__(model, config, adapter_name)
-#        print ("in init")
+        print ("Initializing MiCA")
 #        self.predefined_matrices_A = config.predefined_matrices_A or {}  # Store initial matrices
 #        print (predefined_matrices_A)
         
